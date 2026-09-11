40a45b61722463876c8edd123c2e102adec7a5ec
Hiru1228 <martinselarge762@gmail.com>
2026-09-11 14:19:46 +0530

    Add files via upload

---
+++ Q2/IT26101928Lab6Q2A.java
@@ -0,0 +1,9 @@
+public class IT26101928Lab6Q2A {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
